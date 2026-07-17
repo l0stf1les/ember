7a9901275514eb21960218bf9035e895f08be6c7
l0stf1les <biggaydude69drpenis@gmail.com>
2026-07-16 23:52:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const tabs = document.querySelectorAll(".tab");
+const pages = document.querySelectorAll(".page");
+const slider = document.querySelector(".slider");
 
+tabs.forEach((tab, index) => {
+
+    tab.addEventListener("click", () => {
+
+        tabs.forEach(t => t.classList.remove("active"));
+        pages.forEach(p => p.classList.remove("active"));
+
+        tab.classList.add("active");
+
+        document
+            .getElementById(tab.dataset.page)
+            .classList.add("active");
+
+        slider.style.transform = `translateX(${index * 100}%)`;
+
+        if (tab.dataset.page === "discord") {
+             window.open("https://discord.gg/getember");
+        }
+
+    });
+
+});
